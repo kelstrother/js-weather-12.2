1ccadd6524076fc852725e359b27a3118a37f121
Kel Strother <kelstrother@gmail.com>
2022-12-02 15:50:11 -0700

    added img folder and displayed images based on current time and weather conditions

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,16 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
   // const cityDetails = data.cityDetails;
   // const weather = data.weather;
-  console.log(data);
   //  destructure propterties
   const { cityDetails, weather } = data;
 
+  console.log(data);
   // update details template
   details.innerHTML = `
     <h5 class="my-3">${cityDetails.EnglishName}</h5>
@@ -18,6 +20,18 @@ const updateUI = (data) => {
       <span>&deg;F</span>
     </div>
   `;
+
+  // update night/day icons
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+  
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+  time.setAttribute("src", timeSrc);
 
   // remove the d-none class if present
   if (card.classList.contains("d-none")) {
